b9eeeb9a7297491baaf9d109b4b39ecfb9756b25
David-Rodden <davidrodden273@gmail.com>
2017-09-01 19:43:00 -0700

    Added AnnounceLoss, TradeInitialWinnings NFANodes

---
+++ src/node_structure/nfa_nodes/AnnounceLoss.java
@@ -0,0 +1,29 @@
+package node_structure.nfa_nodes;
+
+import node_structure.NFAHandler;
+import node_structure.NFANode;
+
+public class AnnounceLoss extends NFANode {
+    private boolean announced;
+
+    protected AnnounceLoss(final NFAHandler handler) {
+        super(handler, "Announcing that we've lost");
+    }
+
+    @Override
+    protected void action() {
+        getHandler().getRef().getKeyboard().typeString("Congratulations - I've rolled " + getHandler().getCurrentRoll());
+        announced = true;
+    }
+
+    @Override
+    protected NFANode determine() {
+        if (!getHandler().getRef().getPlayers().closest(getHandler().getCurrentTrader()).exists()) return getFailure();
+        return announced ? getSuccess() : null;
+    }
+
+    @Override
+    protected void transition() {
+        announced = false;
+    }
+}
---
+++ src/node_structure/nfa_nodes/TradeInitialWinnings.java
@@ -0,0 +1,31 @@
+package node_structure.nfa_nodes;
+
+import node_structure.NFAHandler;
+import node_structure.NFANode;
+import org.osbot.rs07.api.model.Player;
+
+public class TradeInitialWinnings extends NFANode {
+    protected TradeInitialWinnings(final NFAHandler handler) {
+        super(handler, "Trading winnings to " + handler.getCurrentTrader());
+    }
+
+    @Override
+    protected void action() {
+        final Player player = getHandler().getRef().getPlayers().closest(getHandler().getCurrentTrader());
+        if (!player.exists()) return;
+        player.interact("Trade with");
+        sleepUntil(() -> getHandler().getRef().getTrade().isFirstInterfaceOpen());
+    }
+
+    @Override
+    protected NFANode determine() {
+        if (getHandler().getRef().getTrade().isFirstInterfaceOpen()) return getSuccess();
+        if (getHandler().getAllottedInTrade() > 15000) return getFailure();
+        return null;
+    }
+
+    @Override
+    protected void transition() {
+
+    }
+}
